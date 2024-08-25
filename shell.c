8ebb86c68e556fc0eb83259437bc6ac18bf833a5
Ruslan <qaqli20033@gmail.com>
2024-08-25 08:26:26 +0000

    Fixed bug

--- shell.c
+++ shell.c
@@ -16,20 +16,20 @@
  */
 char *get_path(void)
 {
-char **env = environ;
-char *path = NULL;
-if (env == NULL)
-return (NULL);
-while (*env != NULL)
-{
-if (strncmp(*env, "PATH=", 5) == 0)
-{
-path = *env + 5;
-return (path);
-}
-env++;
-}
-return (NULL);
+	char **env = environ;
+	char *path = NULL;
+	if (env == NULL)
+	return (NULL);
+	while (*env != NULL)
+	{
+	if (strncmp(*env, "PATH=", 5) == 0)
+	{
+	path = *env + 5;
+	return (path);
+	}
+	env++;
+	}
+	return (NULL);
 }
 
 
