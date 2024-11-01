e48237bfd5d5663327d6c7a5b9b8de5a440fdbee
Islam <9337@holbertonstudents.com>
2024-11-01 10:16:48 +0000

    Fix betty error

--- shell.c
+++ shell.c
@@ -18,16 +18,17 @@ char *get_path(void)
 {
 	char **env = environ;
 	char *path = NULL;
+
 	if (env == NULL)
-	return (NULL);
+		return (NULL);
 	while (*env != NULL)
 	{
-	if (strncmp(*env, "PATH=", 5) == 0)
-	{
-	path = *env + 5;
-	return (path);
-	}
-	env++;
+		if (strncmp(*env, "PATH=", 5) == 0)
+		{
+			path = *env + 5;
+			return (path);
+		}
+		env++;
 	}
 	return (NULL);
 }
@@ -45,33 +46,34 @@ char *get_full_path(char *arg, int *status)
 	char *PATH;
 	char *dir;
 	char *full_path;
+
 	if (access(arg, F_OK) == 0)
 	{
-	full_path = malloc(strlen(arg) + 1);
-	strcpy(full_path, arg);
-	return (full_path);
+		full_path = malloc(strlen(arg) + 1);
+		strcpy(full_path, arg);
+		return (full_path);
 	}
 	if (get_path() == NULL)
 	{
-	fprintf(stderr, "./hsh: 1: %s: not found\n", arg);
-	*status = 127;
-	return (NULL);
+		fprintf(stderr, "./hsh: 1: %s: not found\n", arg);
+		*status = 127;
+		return (NULL);
 	}
 	PATH = strdup(get_path());
 	dir = strtok(PATH, ":");
 	while (dir)
 	{
-	full_path = malloc(strlen(dir) + strlen(arg) + 2);
-	strcpy(full_path, dir);
-	strcat(full_path, "/");
-	strcat(full_path, arg);
-	if (access(full_path, F_OK) == 0)
-	{
-	free(PATH);
-	return (full_path);
-	}
-	free(full_path);
-	dir = strtok(NULL, ":");
+		full_path = malloc(strlen(dir) + strlen(arg) + 2);
+		strcpy(full_path, dir);
+		strcat(full_path, "/");
+		strcat(full_path, arg);
+		if (access(full_path, F_OK) == 0)
+		{
+			free(PATH);
+			return (full_path);
+		}
+		free(full_path);
+		dir = strtok(NULL, ":");
 	}
 	fprintf(stderr, "./hsh: 1: %s: not found\n", arg);
 	*status = 127;
@@ -90,16 +92,17 @@ void set_argv(char *buffer, char ***argv)
 	size_t i;
 	char *arg;
 	char *copy = strdup(buffer);
+
 	arg = strtok(copy, " \n");
 	for (argc = 0; arg; argc++)
-	arg = strtok(NULL, " \n");
+		arg = strtok(NULL, " \n");
 	free(copy);
 	*argv = malloc(sizeof(char *) * (argc + 1));
 	arg = strtok(buffer, " \n");
 	for (i = 0; arg; i++)
 	{
-	(*argv)[i] = arg;
-	arg = strtok(NULL, " \n");
+		(*argv)[i] = arg;
+		arg = strtok(NULL, " \n");
 	}
 	(*argv)[i] = NULL;
 }
@@ -113,18 +116,19 @@ void execute(char **argv, int *status)
 {
 	pid_t pid;
 	char *command;
+
 	if (argv[0] == NULL)
-	return;
+		return;
 	command = get_full_path(argv[0], status);
 	if (command == NULL)
-	return;
+		return;
 	pid = fork();
 	if (pid == 0)
-	execve(command, argv, environ);
+		execve(command, argv, environ);
 	else
 	{
-	wait(NULL);
-	free(command);
+		wait(NULL);
+		free(command);
 	}
 }
 /**
@@ -139,17 +143,18 @@ int main(void)
 	size_t size;
 	ssize_t read;
 	int status = 0;
+
 	while (1)
 	{
-	buffer = NULL;
-	size = 0;
-	read = getline(&buffer, &size, stdin);
-	if (read == -1)
-	break;
-	set_argv(buffer, &argv);
-	execute(argv, &status);
-	free(argv);
-	free(buffer);
+		buffer = NULL;
+		size = 0;
+		read = getline(&buffer, &size, stdin);
+		if (read == -1)
+			break;
+		set_argv(buffer, &argv);
+		execute(argv, &status);
+		free(argv);
+		free(buffer);
 	}
 	free(buffer);
 	exit(status);
