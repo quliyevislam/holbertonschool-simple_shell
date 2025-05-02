bf893c79b059f59a57de288c5633d0007fbfc4a1
quliyevislam <9337@holbertonstudents.com>
2025-05-02 19:19:40 +0400

    Fi

--- shell.c
+++ shell.c
@@ -42,17 +42,15 @@ char *search_path_for_command(char *command, int *status)
 	if (strlen(command) == 0)
 		return (NULL);
 
-	if (access(command, F_OK) == 0)
+	if (access(command, F_OK) == 0 && (command[0] == '/' || command[1] == '/'))
 	{
 		full_path = strdup(command);
 		return (full_path);
 	}
-
 	path = get_path();
 	if (path == NULL)
 	{
-		*status = 127;
-		fprintf(stderr, "./hsh: 1: %s: not found\n", command);
+		*status = 127, fprintf(stderr, "./hsh: 1: %s: not found\n", command);
 		return (NULL);
 	}
 	path_copy = strdup(path);
@@ -60,16 +58,13 @@ char *search_path_for_command(char *command, int *status)
 	while (dir)
 	{
 		full_path = malloc(strlen(dir) + strlen(command) + 2);
-		strcpy(full_path, dir);
-		strcat(full_path, "/");
-		strcat(full_path, command);
+		strcpy(full_path, dir), strcat(full_path, "/"), strcat(full_path, command);
 		if (access(full_path, F_OK) == 0)
 		{
 			free(path_copy);
 			return (full_path);
 		}
-		free(full_path);
-		dir = strtok(NULL, ":");
+		free(full_path), dir = strtok(NULL, ":");
 	}
 	fprintf(stderr, "./hsh: 1: %s: not found\n", command);
 	*status = 127;
