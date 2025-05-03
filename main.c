9e8fc73f1054518d6d61c7fe4a19802e7accea05
quliyevislam <9337@holbertonstudents.com>
2025-05-03 13:28:10 +0400

    Fix

--- main.c
+++ main.c
@@ -10,34 +10,36 @@ int main(void)
 
 	while (1)
 	{
-		line = NULL;
-		argv = NULL;
+		line = NULL, argv = NULL;
 		input_length = getline(&line, &buffer_length, stdin);
 		if (input_length == -1)
 		{
-			free(line), break;
+			free(line);
+			break;
 		}
 		argv = string_to_words_array(line, &status);
 		if (argv == NULL)
 		{
-			free(argv), free(line), continue;
+			free(argv), free(line);
+			continue;
 		}
 		if (strcmp(argv[0], "env") == 0)
 		{
-			print_env(), free(line), free(argv), continue;
+			print_env(), free(line), free(argv);
+			continue;
 		}
 		if (strcmp(argv[0], "exit") == 0)
 		{
-			free(argv), free(line), break;
+			free(argv), free(line);
+			break;
 		}
 		argv[0] = search_path_for_command(argv[0], &status);
 		if (argv[0] == NULL)
 		{
-			free(argv), free(line), continue;
+			free(argv), free(line);
+			continue;
 		}
-
-		status = fork_and_execute(argv);
-		free(argv[0]), free(argv), free(line);
+		status = fork_and_execute(argv), free(argv[0]), free(argv), free(line);
 	}
 	exit(status);
 }
